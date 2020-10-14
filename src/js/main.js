f09de302c69e8b2a60dcf3ee8e153a989bc10bac
James Boncek <jboncek@gmail.com>
2020-10-13 23:28:25 -0500

    update route updates route file

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
-var paintRouteArray = function(routeArray){
+var routeArray = [];
+
+var paintRouteArray = function(){
   routeArray.forEach((hold, index) => {
 
   var elementId = hold.substring(0, 2);
@@ -13,6 +15,10 @@ var paintRouteArray = function(routeArray){
   });
 };
 
+var setupRoute = function(route){
+  paintRouteArray();
+}
+
 var getRouteName = function(){
   const queryString = window.location.search;
 
@@ -26,7 +32,7 @@ var getRoute = function(routename, callback){
      type: "GET",
      url: "api/route.php?routename=" + routename,
      success: function(responseArray) {
-       route = responseArray;
+       routeArray = responseArray;
 
        callback(responseArray);
        // route[routename] = response;
@@ -46,7 +52,7 @@ $( document ).ready(function() {
     var routename = getRouteName();
 
     if(routename){
-      getRoute(routename, paintRouteArray);
+      getRoute(routename, setupRoute);
     }
 
     $('#holdModal').on('show.bs.modal', function (event) {
@@ -82,18 +88,24 @@ $( document ).ready(function() {
 
         var routename = getRouteName();
 
-        getRoute(routename, paintRouteArray);
+        routeArray.push(position + hand);
 
-        route[routename].push(position + hand);
+        console.log(routeArray);
 
-        console.log(route);
+        paintRouteArray(routeArray);
 
-        paintRouteArray(route[routename]);
+        var payload = {};
+
+        payload[routename] = routeArray ;
+
+        console.log(payload);
+
+        var jsonPayload = JSON.stringify(payload);
 
         $.ajax({
            type: "PUT",
-           url: "/api/route.php",
-           data: route,
+           url: "api/route.php",
+           data: jsonPayload,
            success: function(response) {
              $("#holdModal").modal('hide');
              //alert(response['response']);
