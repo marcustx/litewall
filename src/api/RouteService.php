41b8a0af6fdd58ce950337f6e7bfced0737abf39
James Boncek <jboncek@gmail.com>
2020-10-15 13:41:20 -0500

    refactored api to be more oop php; a couple bug fixes too

---
+++ src/api/RouteService.php
@@ -0,0 +1,100 @@
+<?php
+
+class RouteService
+{
+  private $_ledWallService;
+
+  private $_input;
+
+  private $_routename;
+
+  public function __construct($ledWallService, $input, $routename)
+  {
+      $this->_ledWallService = $ledWallService;
+      $this->_input = $input;
+      $this->_routename = $routename;
+  }
+
+  public function getRoute()
+  {
+    if (strlen($this->_routename) == 0) {
+
+      $this->_ledWallService->wallOff();
+
+      return;
+    }
+
+    $filename = "../routes/".$this->_routename;
+
+    if(filesize($filename) > 0)
+    {
+      $handle = fopen($filename, "r") or die ("Unable to Open File");
+
+      $contents = trim(fread($handle, filesize($filename)));
+
+      fclose($handle);
+
+      $routeArray = explode(",",$contents);
+
+      $this->_ledWallService->updateWall($routeArray);
+
+      $jsonResponse = json_encode ( $routeArray, JSON_PRETTY_PRINT );
+
+      header('content-type: application/json; charset=UTF-8');
+
+      print_r ( $jsonResponse );
+    }
+    else
+    {
+      $emptyArray = [];
+
+      $jsonResponse = json_encode ( $emptyArray, JSON_PRETTY_PRINT );
+
+      header('content-type: application/json; charset=UTF-8');
+
+      print_r ( $jsonResponse );
+    }
+  }
+
+  public function createRoute()
+  {
+    $fileKey = "../routes/".$this->_input;
+
+    $routeFile = fopen($fileKey, "w") or die ("Unable to Open File");
+
+    fwrite($routeFile, "");
+
+    fclose($routeFile);
+  }
+
+  public function updateRoute()
+  {
+    foreach($this->_input as $fileName=>$routeArray)
+    {
+      $fileKey = "../routes/".$fileName;
+
+      $this->saveRouteFile($fileKey, $routeArray);
+
+      $this->_ledWallService->updateWall($routeArray);
+    }
+  }
+
+  public function deleteRoute()
+  {
+    $filekey = "../routes/".$this->_input;
+
+    unlink($filekey);
+  }
+
+  private function saveRouteFile($fileKey, $routeArray){
+
+    $newValues = implode($routeArray, ",");
+
+    $routeFile = fopen($fileKey, "w") or die ("Unable to Open File");
+
+    fwrite($routeFile, $newValues );
+
+    fclose($routeFile);
+  }
+}
+ ?>
---
+++ src/litewall/LedWallService.php
@@ -0,0 +1,77 @@
+<?php
+
+class LedWallService implements ILedWallService
+{
+  private $_config;
+
+  public function __construct($config)
+  {
+      $this->_config = $config;
+  }
+
+  public function wallOff(){
+    $stringCommand = $this->getBaseNeoPixelCommand();
+
+    $stringCommand .= "pixels.fill((0, 0, 0))\"";
+
+    echo `$stringCommand`;
+  }
+
+  public function updateWall($routeArray)
+  {
+    $stringCommand = $this->getBaseNeoPixelCommand();
+
+    foreach ($routeArray as $hold)
+    {
+      $holdArray = str_split($hold, 2);
+
+      $holdPosition = $holdArray[0];
+
+      $holdHand = $holdArray[1];
+
+      $ledId = $this->getLedId($holdPosition);
+
+      $ledColor = $this->getLedColor($holdHand);
+
+      $stringCommand .= "pixels[".$ledId."] = ".$ledColor.";";
+    }
+
+    $stringCommand .= "\"";
+
+    echo `$stringCommand`;
+  }
+
+  private function getLedColor($holdHand)
+  {
+    if($holdHand == "L"){
+      return $this->_config["left_hand_led_rgb"];
+    }
+
+    if($holdHand == "R"){
+      return $this->_config["right_hand_led_rgb"];
+    }
+
+    if($holdHand == "M"){
+      return $this->_config["match_hand_led_rgb"];
+    }
+  }
+
+  private function getLedId($holdPosition){
+    return $this->_config["led_id_map"]->$holdPosition;
+  }
+
+  private function getBaseNeoPixelCommand(){
+    $stringCommand = "sudo python3 -c \"import board, neopixel; ";
+    $stringCommand .= "pixels = neopixel.NeoPixel(";
+    $stringCommand .= $this->_config['neoPixelPin'];
+    $stringCommand .= ", ";
+    $stringCommand .= $this->_config['neoPixelNumberOfPixels'];
+    $stringCommand .= ", brightness=";
+    $stringCommand .= $this->_config['neoPixelBrightness'];
+    $stringCommand .= ");";
+
+    return $stringCommand;
+  }
+}
+
+?>
